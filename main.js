7849b4125ada67e322b981c5c4eb4634aaed3ac8
Mark Tom <122617440+Co-lest@users.noreply.github.com>
2024-12-03 09:50:54 +0300

    exec

---
+++ main.js
@@ -0,0 +1,15 @@
+import os from "os";
+
+(function compVars (){
+  const operatingSystem = os.platform();
+  const architecture = os.arch();
+  const release = os.release();
+
+  console.log(operatingSystem, architecture, release);
+
+  if () {
+    
+  }
+})();
+
+
