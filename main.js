55337d07c02bf22bab8c1c6f72faba2d28d95ac7
Mark Tom <122617440+Co-lest@users.noreply.github.com>
2024-12-09 21:46:39 +0300

    staticfiles

--- main.js
+++ main.js
@@ -1,88 +1,219 @@
+// import http from "http";
+// import "dotenv/config";
+// import fs from "fs/promises";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// // import { execExportWin, execRemoveWin } from "./Utils/win32.js";
+// // import { execExportMac, execRemoveMac} from "./Utils/mac.js"
+// // import { execExportLin, execRemoveLin } from "./Utils/linux.js";
+// import { WebSocketServer } from "ws";
+
+// const port = process.env.PORT;
+
+// // let programsObj;
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const server = http.createServer(async (req, res) => {
+//   try {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+//     console.log(`Incoming request: ${req.method}, ${req.url}`);
+
+//     let filePath;
+
+//     if (req.method === "GET") {
+//       if (req.url === "/") {
+//         filePath = `index.html`;
+//         // console.log(filePath);
+
+//         const fullPath = path.join(__dirname, "public", filePath);
+//         console.log(fullPath);
+
+//         const content = await fs.readFile(fullPath);
+//         const ext = path.extname(filePath);
+//         const contentType =
+//           {
+//             ".html": "text/html",
+//             ".css": "text/css",
+//             ".js": "text/javascript",
+//           }[ext] || "text/plain";
+
+//         res.writeHead(200, { "Content-Type": contentType });
+//         res.end(content);
+//       } else {
+//         console.log(req.url);
+//         throw new Error("Filepath not allowed!");
+//       }
+//     }
+//   } catch (err) {
+//     console.error("An error occurred when creating a server!", err);
+//     res.writeHead(500, { "Content-Type": "text/plain" });
+//     res.end("Internal Server Error");
+//   }
+// });
+
+// server.listen(port, () => {
+//   console.log(`Server listenig on port ${port}`);
+// });
+
+// const wss = new WebSocketServer({ server });
+
+// wss.on("connection", (ws) => {
+//   console.log(`A client connected`);
+//   // compVars();
+//   ws.on("message", (message) => {
+//     console.log(message.data);
+//   });
+// });
+
+// const compVars = () => {
+//   const operatingSystem = os.platform();
+//   const architecture = os.arch();
+//   const release = os.release();
+//   console.log(operatingSystem, architecture, release);
+//   let command;
+//   let programName = "Microsoft Edge";
+//   if (operatingSystem === 'win32') {
+//     command = `wmic product where name='${programName}' call uninstall`;
+//     programsObj = execExportWin();
+//     execRemoveWin(command, programName);
+//   }else if (operatingSystem === "darwin") {
+//       command = `osascript -e 'do shell script "sudo rm -rf /Applications/${programName}.app"' with administrator privileges`;
+//       execExportMac();
+//       execRemoveMac(command, programName);
+//     } else if(operatingSystem === "linux") {
+//       command = `sudo apt remove ${programName}`;
+//       execExportLin();
+//       execRemoveLin(command, programName)
+//     }
+// };
+
+// const server = http.createServer(async (req, res) => {
+//   try {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+//     if (req.method === "GET") {
+//       let filePath;
+//       if (req.url === "/") {
+//         filePath = "/index.html";
+//       } else {
+//         filePath = req.url;
+//       }
+
+//       const fullPath = path.join(__dirname, "public", filePath);
+//       const content = await fs.readFile(fullPath);
+
+//       console.log(fullPath);
+
+//       const ext = path.extname(filePath);
+//       const contentType = {
+//         ".html": "text/html",
+//         ".css": "text/css",
+//         ".js": "text/javascript",
+//       }[ext] || "text/plain";
+
+//       res.writeHead(200, { "Content-Type": contentType });
+//       res.end(content);
+//     }
+
+//   } catch (err) {
+//     console.error("An error occurred when creating a server!", err);
+//     res.writeHead(500, { "Content-Type": "text/plain" });
+//     res.end("Internal Server Error");
+//   }
+// });
+
+
+// server.js
+
 import http from "http";
 import "dotenv/config";
 import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
-// import { execExportWin, execRemoveWin } from "./Utils/win32.js";
-// import { execExportMac, execRemoveMac} from "./Utils/mac.js"
-// import { execExportLin, execRemoveLin } from "./Utils/linux.js";
 import { WebSocketServer } from "ws";
 
 const port = process.env.PORT;
-
-let programsObj;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = http.createServer(async (req, res) => {
   try {
+    console.log(`Incoming Request: ${req.method} ${req.url}`); 
+    // Log request details for easier debugging
+
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+    let filePath;
+
     if (req.method === "GET") {
-      let filePath = "/index.html";
-      // if (req.url === "/") {
-      //   filePath = "/index.html";
-      // } else {
-      //   filePath = req.url;
-      // }
+      if (req.url === "/") {
+        filePath = `index.html`;
+        const fullPath = path.join(__dirname, "public", filePath);
+        console.log(fullPath);
 
-      const fullPath = path.join(__dirname, "public", filePath); 
-      const content = await fs.readFile(fullPath); 
+        const content = await fs.readFile(fullPath);
+        const ext = path.extname(filePath);
+        const contentType =
+          {
+            ".html": "text/html",
+            ".css": "text/css",
+            ".js": "text/javascript",
+          }[ext] || "text/plain";
 
-      const ext = path.extname(filePath);
-      const contentType = {
-        ".html": "text/html",
-        ".css": "text/css",
-        ".js": "text/javascript",
-      }[ext] || "text/plain";
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(content);
+      } else {
+        console.log("Serving static file:", req.url); // Log static file requests
+        filePath = req.url.substring(1);
+        const fullPath = path.join(__dirname, "public", filePath);
+        console.log(`Static file file path ${fullPath}`);
 
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content); 
-      connectWebserver();
+        try {
+          const content = await fs.readFile(fullPath);
+          const ext = path.extname(filePath);
+          const contentType =
+            {
+              ".html": "text/html",
+              ".css": "text/css",
+              ".js": "text/javascript",
+            }[ext] || "text/plain";
+
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(content);
+        } catch (err) {
+          console.error(`Error serving static file: ${req.url}`, err);
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("File Not Found");
+        }
+      }
     }
-
   } catch (err) {
     console.error("An error occurred when creating a server!", err);
     res.writeHead(500, { "Content-Type": "text/plain" });
-    res.end("Internal Server Error"); 
+    res.end("Internal Server Error");
   }
 });
 
-const connectWebserver = () => {
-  const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({ server });
 
-  wss.addListener("connection", (ws) => {
-    console.log(`A client connected`);
-    // compVars();
-    ws.addEventListener("message", () => {});
+wss.on("connection", (ws) => {
+  console.log(`A client connected`);
+  // compVars();
+  ws.on("message", (message) => {
+    console.log(message.data);
   });
-};
-
-const compVars = () => {
-  // const operatingSystem = os.platform();
-  // const architecture = os.arch();
-  // const release = os.release();
-  // console.log(operatingSystem, architecture, release);
-  // let command;
-  // let programName = "Microsoft Edge";
-  // if (operatingSystem === 'win32') {
-  //   command = `wmic product where name='${programName}' call uninstall`;
-  //   programsObj = execExportWin();
-  //   execRemoveWin(command, programName);
-  // }else if (operatingSystem === "darwin") {
-  //     command = `osascript -e 'do shell script "sudo rm -rf /Applications/${programName}.app"' with administrator privileges`;
-  //     execExportMac();
-  //     execRemoveMac(command, programName);
-  //   } else if(operatingSystem === "linux") {
-  //     command = `sudo apt remove ${programName}`;
-  //     execExportLin();
-  //     execRemoveLin(command, programName)
-  //   }
-};
+});
 
 server.listen(port, () => {
-  console.log(`Server listenig on port ${port}`);
+  console.log(`Server listening on port: ${port}`);
 });
--- public/index.js
+++ public/index.js
@@ -1,33 +1,35 @@
-import { execExportWin } from "../Utils/win32.js";
-import { execExportMac } from "../Utils/mac.js";
-import { execExportLin } from "../Utils/linux.js";
+import { execExportWin } from "../utils/win32.js";
+import { execExportMac } from "../utils/mac.js";
+import { execExportLin } from "../utils/linux.js";
 
 const par = document.querySelector("#par");
-
-window.addEventListener("load", () => {
-  getOs();
-  connectWebServer();
-});
+const uninstallContainer = document.querySelector("#uninstall-container");
 
 let programs;
 
-const getOs = () => {
+(function getOs(){
   const plat1 = navigator.platform;
 
   if (plat1.indexOf("Win") !== -1) {
     programs = execExportWin();
+    connectWebServer();
     if (programs) {
       console.log(programs);
       par.textContent = programs;
+      uninstallContainer.style.display = "block";
     } else {
       console.log(`Unaniona matako yako!`);
     }
   } else if (plat1.indexOf("Mac") !== -1) {
     programs = execExportMac();
     par.textContent = programs;
+    uninstallContainer.style.display = "block";
+    connectWebServer();
   } else if (plat1.indexOf("Linux") !== -1) {
     programs = execExportLin();
     par.textContent = programs;
+    uninstallContainer.style.display = "block";
+    connectWebServer();
   } else if (plat1.indexOf("Android") !== -1) {
     return "Android OS no permissions to remove programs! Sorry";
   } else if (plat1.indexOf("iPhone") !== -1 || plat1.indexOf("iPad") !== -1) {
@@ -35,10 +37,11 @@ const getOs = () => {
   } else {
     return "Unknown OS";
   }
-};
+})();
 
 const connectWebServer = () => {
   let protocol = "";
+  console.log(window.location.host, "\n", window.location.protocol);
   if (window.location.protocol === "https:") {
     protocol = "ws";
   } else {
@@ -49,6 +52,7 @@ const connectWebServer = () => {
 
   socket.addEventListener("open", () => {
     console.log(`Connected to websocket`);
+    socket.send("Connected to the websocket!");
   });
 
   socket.addEventListener("error", () => {
